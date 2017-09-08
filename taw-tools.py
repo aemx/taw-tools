be53bc79b6d48d5d04d4affb93eefd2b6c9176dd
Spencer Gunning <swcgunning@gmail.com>
2017-09-08 13:37:15 -0400

    [0.0.4-b3] Added a datatype arguement in scraper

--- taw-tools.py
+++ taw-tools.py
@@ -8,61 +8,61 @@ def wprint(string):
 def mean(li):
     return int(round(np.mean(li)))
 
-def scrape(url, selector, chars):
+def scrape(url, selector, datatype, chars):
     try:
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'lxml')
         select = soup.select(selector)
         try:
-            return (select[0].text).strip(chars)
+            return datatype((select[0].text).strip(chars))
         except IndexError:
-            return (select.text).strip(chars)
+            return datatype((select.text).strip(chars))
     except:
         wprint('Something went wrong')
-        
-wx_rtmpAr = [float(scrape(
+
+wx_rtmpAr = [scrape(
     'https://forecast.weather.gov/MapClick.php?lat=40.7387&lon=-74.1955',
-    '#current_conditions-summary > p.myforecast-current-lrg', '°F'
-)), float(scrape(
+    '#current_conditions-summary > p.myforecast-current-lrg', float, '°F'
+), scrape(
     'https://www.wunderground.com/personal-weather-station/dashboard?ID=KNJNEWAR10',
-    '#curTemp > span > span.wx-value', ''
-)), float(scrape(
+    '#curTemp > span > span.wx-value', float, ''
+), scrape(
     'https://darksky.net/forecast/40.7387,-74.1955',
-    '#title > span.currently > span.desc.swap > span.temp.swip', '˚'
-)), float(scrape(
+    '#title > span.currently > span.desc.swap > span.temp.swip', float, '˚'
+), scrape(
     'https://www.accuweather.com/en/us/newark-nj/07103/current-weather/2702_pc',
-    '#detail-now > div > div.forecast > div.info > div > span.large-temp', '°'
-))]
+    '#detail-now > div > div.forecast > div.info > div > span.large-temp', float, '°'
+)]
 
 wx_rtmp = mean(wx_rtmpAr)
 
 wx_stat = scrape(
     'https://forecast.weather.gov/MapClick.php?lat=40.7387&lon=-74.1955',
-    '#current_conditions-summary > p.myforecast-current', ''
+    '#current_conditions-summary > p.myforecast-current', str, ''
 )
 
-wx_atmpAr = [float(scrape(
+wx_atmpAr = [scrape(
     'https://www.wunderground.com/personal-weather-station/dashboard?ID=KNJNEWAR10',
-    '#curFeel > span.wx-data > span.wx-value', ''
-)), float(scrape(
+    '#curFeel > span.wx-data > span.wx-value', float, ''
+), scrape(
     'https://www.accuweather.com/en/us/newark-nj/07103/current-weather/2702_pc',
-    '#detail-now > div > div.forecast > div.info > div > span.small-temp', 'aeFlR®°'
-))]
+    '#detail-now > div > div.forecast > div.info > div > span.small-temp', float, 'aeFlR®°'
+)]
 
 wx_atmp = mean(wx_atmpAr)
 
-wx_windAr = [float(scrape(
+wx_windAr = [scrape(
     'https://www.wunderground.com/personal-weather-station/dashboard?ID=KNJNEWAR10',
-    '#windCompassSpeed > h4 > span', ''
-)), float(scrape(
+    '#windCompassSpeed > h4 > span', float, ''
+), scrape(
     'https://darksky.net/forecast/40.7387,-74.1955',
-    '#currentDetails > div.wind > span.val.swap > span.num.swip', ''
-))]
+    '#currentDetails > div.wind > span.val.swap > span.num.swip', float, ''
+)]
 
 wx_wind = mean(wx_windAr)
 
 if wx_wind >= 25: wx_wist = ' ╱ Windy'
-elif wx_wind >= 15: wx_wist = ' ╱ Light wind'
+elif wx_wind >= 15: wx_wist = ' ╱ Light Wind'
 else: wx_wist = ''
 
 print('\n' + \
